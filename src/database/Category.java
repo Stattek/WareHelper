a38456f949959c9dba29e4771bd84e3a58e31fa2
Stattek <stattek1@pm.me>
2025-03-05 19:49:22 -0600

    Use name as identifier for Category.

--- src/database/Category.java
+++ src/database/Category.java
@@ -4,8 +4,6 @@ package database;
  * Class that represents a Category for an Item.
  */
 public class Category {
-    private static int nextId = 0;
-    private int id;
     private String name;
 
     /**
@@ -16,8 +14,5 @@ public class Category {
      */
     public Category(String name) {
         this.name = name;
-        this.id = nextId;
-        // increment next possible ID.
-        nextId++;
     }
 }
