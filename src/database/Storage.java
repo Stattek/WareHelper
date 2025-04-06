eff9abc2dab5042fab848f97c0eb127be9353e01
Stattek <stattek1@pm.me>
2025-04-06 04:00:09 -0500

    Update Storage interface.

--- src/database/Storage.java
+++ src/database/Storage.java
@@ -1,7 +1,9 @@
 package database;
 
+import java.util.List;
+
 public interface Storage extends AutoCloseable {
     public boolean write(String data);
 
-    public String read(int id);
+    public String read(String tableName, int id, List<String> keys);
 }
