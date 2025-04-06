caf16bd9cee3da2d8d19c2bf0af3269e1689ceda
Stattek <stattek1@pm.me>
2025-04-06 04:51:18 -0500

    Add function.

--- src/database/Storage.java
+++ src/database/Storage.java
@@ -3,6 +3,7 @@ package database;
 import java.util.List;
 
 public interface Storage extends AutoCloseable {
+    // TODO: Might be best to just have functions for read, write, update, and delete
     public boolean write(String data);
 
     public String read(String tableName, int id, List<String> keys);
