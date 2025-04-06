f084b060d0c7c979490055b2245d75967cc30c43
Stattek <stattek1@pm.me>
2025-04-05 22:55:17 -0500

    Create Preference class.

---
+++ src/database/items/Preference.java
@@ -0,0 +1,33 @@
+package database.items;
+
+public class Preference {
+    private int sellWithinNumDays; // number of days to sell the item in
+    private int lowInventoryThreshold; // number of items before it is considered low inventory
+    private double promotionPercentOff; // percent off promotion
+
+    /* Getters and Setters */
+
+    public int getSellWithinNumDays() {
+        return sellWithinNumDays;
+    }
+
+    public void setSellWithinNumDays(int sellWithinNumDays) {
+        this.sellWithinNumDays = sellWithinNumDays;
+    }
+
+    public int getLowInventoryThreshold() {
+        return lowInventoryThreshold;
+    }
+
+    public void setLowInventoryThreshold(int lowInventoryThreshold) {
+        this.lowInventoryThreshold = lowInventoryThreshold;
+    }
+
+    public double getPromotionPercentOff() {
+        return promotionPercentOff;
+    }
+
+    public void setPromotionPercentOff(double promotionPercentOff) {
+        this.promotionPercentOff = promotionPercentOff;
+    }
+}
