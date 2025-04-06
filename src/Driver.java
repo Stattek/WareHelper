fe67ca70c87eec441516235efd5a31b4d27157f2
Stattek <stattek1@pm.me>
2025-04-06 00:52:43 -0500

    Comment out old code.

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,4 @@
 import java.sql.*;
-import database.DatabaseConnection;
-import database.DatabaseConnection.DatabaseQueryResult;
 
 /**
  * Driver class for running WareHelper.
@@ -10,39 +8,42 @@ public class Driver {
     private static final boolean isUsingLocalDatabase = true;
 
     public static void main(String[] args) {
-        // connect to database
-        DatabaseConnection database = null;
-        try {
-            Class.forName("oracle.jdbc.OracleDriver");
-            // we connect to a database named "warehelper" as the user "testuser"
-            // NOTE: this database has to be setup by the user before we can connect to it.
-            // Is is worth it to set up ourselves?
-            String url = "jdbc:mysql://localhost:3306/warehelper";
-            String username = "testuser";
-            String password = "password";
-            if (!isUsingLocalDatabase) {
-                url = "jdbc:oracle:thin:@10.110.10.90:1521:oracle";
-                username = "IT326T03";
-                password = "reach98";
-            }
-            database = new DatabaseConnection(url, username, password);
-            DatabaseQueryResult queryResult = database.performQuery("select * from ItemTest");
-            ResultSet resultSet = queryResult.getResultSet();
+        // // connect to database
+        // DatabaseConnection database = null;
+        // try {
+        // Class.forName("oracle.jdbc.OracleDriver");
+        // // we connect to a database named "warehelper" as the user "testuser"
+        // // NOTE: this database has to be setup by the user before we can connect to
+        // it.
+        // // Is is worth it to set up ourselves?
+        // String url = "jdbc:mysql://localhost:3306/warehelper";
+        // String username = "testuser";
+        // String password = "password";
+        // if (!isUsingLocalDatabase) {
+        // url = "jdbc:oracle:thin:@10.110.10.90:1521:oracle";
+        // username = "IT326T03";
+        // password = "reach98";
+        // }
+        // database = new DatabaseConnection(url, username, password);
+        // DatabaseQueryResult queryResult = database.performQuery("select * from
+        // ItemTest");
+        // ResultSet resultSet = queryResult.getResultSet();
 
-            int itemId;
-            String name;
-            while (resultSet.next()) {
-                itemId = resultSet.getInt("ItemId");
-                name = resultSet.getString("Name").trim();
-                System.out.println("ItemId : " + itemId + " Name : " + name);
-            }
+        // int itemId;
+        // String name;
+        // while (resultSet.next()) {
+        // itemId = resultSet.getInt("ItemId");
+        // name = resultSet.getString("Name").trim();
+        // System.out.println("ItemId : " + itemId + " Name : " + name);
+        // }
 
-            queryResult.close();
-            database.close();
-        } catch (Exception e) {
-            // TODO: stack traces should not be in production code, but are useful in dev
-            e.printStackTrace();
-        }
+        // queryResult.close();
+        // database.close();
+        // } catch (Exception e) {
+        // // TODO: stack traces should not be in production code, but are useful in dev
+        // e.printStackTrace();
+        // }
+
         System.out.println("Hello World!");
     }
 }
