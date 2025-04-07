c35f12bd30dd84007e3c0b74d510dac29c7453b3
Stattek <stattek1@pm.me>
2025-04-07 01:25:48 -0500

    Update ObjectFactory.

--- src/database/items/ObjectFactory.java
+++ src/database/items/ObjectFactory.java
@@ -5,10 +5,16 @@ import java.util.Map;
 public abstract class ObjectFactory {
     protected ConvertableObject convertableObject;
 
-    public abstract Item createItem(Map<String, String> data);
-
     public abstract Bundle createBundle();
 
     public abstract Category createCategory();
+
+    public abstract DateInfo createDateInfo();
+
+    public abstract EconomyInfo createEconoomyInfo();
+
+    public abstract Item createItem(Map<String, String> data);
+
+    public abstract Preference createPreferenceInfo();
 
 }
