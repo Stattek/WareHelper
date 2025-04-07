2e24c90bb38c3d14c2678972180e464270ad8eb3
Stattek <stattek1@pm.me>
2025-04-06 19:29:06 -0500

    Update function signature.

--- src/database/Storage.java
+++ src/database/Storage.java
@@ -1,10 +1,12 @@
 package database;
 
 import java.util.List;
+import java.util.Map;
 
 public interface Storage extends AutoCloseable {
-    // TODO: Might be best to just have functions for read, write, update, and delete
+    // TODO: Might be best to just have functions for read, create, update, and
+    // delete
     public boolean write(String data);
 
-    public String read(String tableName, int id, List<String> keys);
+    public Map<String, String> read(String tableName, int id, List<String> keys);
 }
