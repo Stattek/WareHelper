e889b02cffa3cec90a515b958de4861795a4305f
Stattek <stattek1@pm.me>
2025-04-11 22:16:20 -0500

    Add ObjectService.

---
+++ src/database/items/ObjectService.java
@@ -0,0 +1,19 @@
+package database.items;
+
+import java.util.Map;
+
+public class ObjectService {
+
+    public static Bundle createBundle(Map<String, String> bundleData) {
+        return null; // TODO: implement
+    }
+
+    public static Category createCategory(Map<String, String> categoryData) {
+        return null; // TODO: implement
+    }
+
+    public static Item createItem(Map<String, String> itemData) {
+        return null; // TODO: implement
+    }
+
+}
