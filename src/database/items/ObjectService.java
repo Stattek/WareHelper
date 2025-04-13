f40d0f393a338cbe973a76c6e1972790a5f8b148
Stattek <stattek1@pm.me>
2025-04-13 02:33:41 -0500

    Implement ObjectService for different creates.

--- src/database/items/ObjectService.java
+++ src/database/items/ObjectService.java
@@ -1,26 +1,63 @@
 package database.items;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ObjectService {
 
-    public static Bundle createBundle(Map<String, String> bundleData) {
-        return null; // TODO: implement
+    public static Bundle createBundle(Map<String, String> bundleData, List<Map<String, String>> itemsData,
+            List<Map<String, String>> itemInnerCategoriesData) {
+        Bundle output = new Bundle();
+
+        try {
+            int bundleId = Integer.parseInt(bundleData.get("BundleId"));
+            double bundleDiscount = Double.parseDouble(bundleData.get("BundleDiscount"));
+            List<Item> items = new ArrayList<>();
+            for (int i = 0; i < itemsData.size(); i++) {
+                items.add(createItem(itemsData.get(i), itemInnerCategoriesData.get(i)));
+            }
+
+            // set values
+            output.setBundleId(bundleId);
+            output.setBundleDiscount(bundleDiscount);
+            output.setItems(items);
+        } catch (Exception e) {
+            // There was an error with converting this data to a Category, throw an error
+            throw new RuntimeException("Could not create Category from read data", e);
+        }
+
+        return output;
     }
 
     public static Category createCategory(Map<String, String> categoryData) {
-        return null; // TODO: implement
+        Category output = new Category();
+
+        try {
+            int categoryId = Integer.parseInt(categoryData.get("CategoryId"));
+            String name = categoryData.get("Name");
+
+            // set values
+            output.setCategoryId(categoryId);
+            output.setName(name);
+        } catch (Exception e) {
+            // There was an error with converting this data to a Category, throw an error
+            throw new RuntimeException("Could not create Category from read data", e);
+        }
+
+        return output;
     }
 
-    public static Item createItem(Map<String, String> itemData) throws RuntimeException {
+    public static Item createItem(Map<String, String> itemData, Map<String, String> innerCategoryData)
+            throws RuntimeException {
         Item output = new Item();
 
         try {
             int itemId = Integer.parseInt(itemData.get("ItemId"));
             String sku = itemData.get("Sku");
             String name = itemData.get("Name");
-            Category category = new Category();
+            Category category = createCategory(innerCategoryData);
             double price = Double.parseDouble(itemData.get("Price"));
             int numItems = Integer.parseInt(itemData.get("NumItems"));
             Date created = Date.valueOf(itemData.get("Created"));
