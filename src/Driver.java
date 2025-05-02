af1b69603bb5af3fa18ece0e8c2ebefcc4ad6623
Stattek <stattek1@pm.me>
2025-05-02 01:52:11 -0500

    Remove color codes for now.

--- src/Driver.java
+++ src/Driver.java
@@ -13,9 +13,6 @@ public class Driver {
 
     private static Controller controller; // controller to communicate with
 
-    private static final String ANSI_RED_COLOR_CODE = "\033[1;31m";
-    private static final String ANSI_RESET_COLOR_CODE = "\033[0m";
-
     /**
      * Retrieves the entire inventory.
      * 
@@ -63,7 +60,7 @@ public class Driver {
                     break;
                 case 2:
                     // read all items
-                    System.out.println(ANSI_RED_COLOR_CODE + controller.readAllItems() + ANSI_RESET_COLOR_CODE);
+                    System.out.println(controller.readAllItems());
                     continueChoice = false;
                     break;
                 default:
