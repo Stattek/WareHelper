8349aa8fa1c7c8607e317bf51be37acc49cdb204
pokopokko <deep06112004@gmail.com>
2025-05-03 21:08:43 -0500

    more documentation

--- src/database/reports/ReportGeneratorFactory.java
+++ src/database/reports/ReportGeneratorFactory.java
@@ -4,20 +4,51 @@ import database.items.*;
 
 import java.util.List;
 
+/**
+ * Factory class responsible for generating various inventory reports
+ */
 public class ReportGeneratorFactory {
+    LowInventoryReport lowInventoryReport = new LowInventoryReport();
+    UnsoldInventoryReport unsoldInventoryReport = new UnsoldInventoryReport();
+    InventoryVolumeReport inventoryVolumeReport = new InventoryVolumeReport();
+
+    /**
+     * Generates a  low inventory report based on the provided categories, items, and bundles.
+     *
+     * @param categories the list of categories to include in the report
+     * @param items      the list of items to include in the report
+     * @param bundles    the list of bundles to include in the report
+     * @return true if the report generation was successful, false otherwise
+     */
     public boolean generateLowInventoryReport(List<Category> categories, List<Item> items, List<Bundle> bundles) {
-        LowInventoryReport lowInventoryReport = new LowInventoryReport();
+
         return lowInventoryReport.generateReport(items, categories, bundles);
     }
 
-    
+    /**
+     * Generates an unsold inventory report based on the provided categories, items,
+     * and bundles.
+     *
+     * @param categories the list of categories to include in the report
+     * @param items      the list of items to include in the report
+     * @param bundles    the list of bundles to include in the report
+     * @return true if the report generation was successful, false otherwise
+     */
     public boolean generateUnsoldInventoryReport(List<Category> categories, List<Item> items, List<Bundle> bundles) {
-        UnsoldInventoryReport unsoldInventoryReport = new UnsoldInventoryReport();
+
         return unsoldInventoryReport.generateReport(items, categories, bundles);
     }
 
+    /**
+     * Generates a inventory volume report based on the provided categories, items, and bundles.
+     *
+     * @param categories the list of categories to include in the report
+     * @param items      the list of items to include in the report
+     * @param bundles    the list of bundles to include in the report
+     * @return true if the report generation was successful, false otherwise
+     */
     public boolean generateInventoryVolumeReport(List<Category> categories, List<Item> items, List<Bundle> bundles) {
-        InventoryVolumeReport inventoryVolumeReport = new InventoryVolumeReport();
+
         return inventoryVolumeReport.generateReport(items, categories, bundles);
     }
 }
